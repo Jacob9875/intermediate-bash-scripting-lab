51492dd548838b8ea4fe3950137f85159348364a
Jacob9875 <jake420clement@gmail.com>
2026-02-04 11:14:35 -0500

    Intermediate Bash

--- sysinfo.sh
+++ sysinfo.sh
@@ -6,7 +6,8 @@ echo "Welcome, select one of the following options using the number keys:"
 echo "1: Show CPU Info"
 echo "2: Show GPU Usage"
 echo "3: Show Log Files"
-echo "4: Exit"
+echo "4: Show VRAM usage"
+echo "5: Exit"
 
 read choice
 
@@ -17,6 +18,8 @@ elif [ "$choice" = "2" ]; then
 elif [ "$choice" = "3" ]; then
   echo "You selected Option 3"
 elif [ "$choice" = "4" ]; then
+  echo "You selected Option 3"
+elif [ "$choice" = "5" ]; then
   echo "Exiting the script"
   exit
 else
